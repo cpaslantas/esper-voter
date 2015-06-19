c3f89fbc6f5193bc9e1042f23ceef2acab649ed3
jmeehan16 <jmeehan16@gmail.com>
2015-06-19 02:04:33 -0400

    minor experiment change

--- experiments/0619/esper-compare-dc.sh
+++ experiments/0619/esper-compare-dc.sh
@@ -15,7 +15,7 @@ do
 		-Doutfile="/home/${DIR}/git/esper-voter/data/out/0619/voltdbadhoc-ramp-1.txt" -Drate="1x${INPUT}" -Dcontestants=50 \
 		-Dthreshold=20000 -Drootdir="/home/${DIR}" -Dbackend="voltdbadhoc" -e
 	ant run -Dthread=1 -Ddir="/home/${DIR}/git/esper-voter/" -Dfile="votes-50-20000_1.txt" \
-		-Doutfile="/home/${DIR}/git/esper-voter/data/out/0619/esper-ramp-1.txt" -Drate="1x${INPUT}" -Dcontestants=50 \
-		-Dthreshold=20000 -Drootdir="/home/${DIR}" -Dbackend="esper" -e
+		-Doutfile="/home/${DIR}/git/esper-voter/data/out/0619/voltdbsp-ramp-1.txt" -Drate="1x${INPUT}" -Dcontestants=50 \
+		-Dthreshold=20000 -Drootdir="/home/${DIR}" -Dbackend="voltdb" -e
 	let INPUT+=100
 done
